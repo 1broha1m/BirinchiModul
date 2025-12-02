81fa1eb3512c34e35d277ddd58adcb818ea37c8e
1broha1m <jorayevas066@gmail.com>
2025-12-02 18:03:25 +0500

    dars2

--- 2dars/2dars/Program.cs
+++ 2dars/2dars/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Assalomu alekum Brothers");
+            Console.WriteLine("Hello world");
         }
     }
 }
