d264f6a7e7b900996a68198e815f4561fec871d8
1broha1m <jorayevas066@gmail.com>
2025-12-05 19:40:58 +0500

    1.3.1-Dars boldi

---
+++ 3-dars/3-dars/3-dars/Program.cs
@@ -0,0 +1,442 @@
+﻿namespace _3_dars;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //1.3.1-_3_dars
+
+        //1.3.1.1 - mawq
+        //for (var i = 1; i <= 20; i++)
+        //{
+        //    Console.Write($"{i}  ");
+        //}
+
+
+        //1.3.1.2 - mawq
+        //for (var i = 25; i <= 100; i++)
+        //{
+        //    Console.Write($"{i}  ");
+        //}
+
+
+        //1.3.1.3 - mawq
+        //for (var i = 12; i <= 200; i++)
+        //{
+        //    Console.Write($"{i}  ");
+        //}
+
+
+        //1.3.1.4 - mawq
+        //for (var i = 15; i >= 2; i--)
+        //{
+        //    Console.Write($"{i}  ");
+        //}
+
+
+        //1.3.1.5 - mawq
+        //for (var i = 120; i >= 23; i--)
+        //{
+        //    Console.Write($"{i}  ");
+        //}
+
+
+        //1.3.1.6 - mawq
+        //for (var i = 10; i <= 50; i++){
+        //    if (i % 2 != 0)
+        //    {
+        //        Console.Write($"{i} ");
+        //    }
+        //}
+
+
+        //1.3.1.7 - mawq
+        //for (var i = 120; i <= 250; i++)
+        //{
+        //    if (i % 2 == 0)
+        //    {
+        //        Console.Write($"{i} ");
+        //    }
+        //}
+
+
+        //1.3.1.8 - mawq
+        //for (var i = 100; i >= 50; i--)
+        //{
+        //    if (i % 2 != 0)
+        //    {
+        //        Console.Write($"{i} ");
+        //    }
+        //}
+
+
+        //1.3.1.9 - mawq
+        //for (var i = 80; i >= 20; i--)
+        //{
+        //    if (i % 2 == 0)
+        //    {
+        //        Console.Write($"{i} ");
+        //    }
+        //}
+
+
+        //1.3.1.10 - mawq
+        //Console.Write("1-sonni kiriting : ");
+        //var firsValue = int.Parse(Console.ReadLine());
+        //Console.Write("2-sonni kiriting : ");
+        //var secondValue = int.Parse(Console.ReadLine());
+        //if (firsValue < secondValue) {
+        //    for (var i = firsValue; i <= secondValue; i++)
+        //    {
+        //        if (i % 5 == 0)
+        //        {
+        //            Console.Write($"{i} ");
+        //        }
+        //    }
+        //} else
+        //{
+        //    Console.Write("1-son 2-sondan kichik bolsin !!!");
+        //}
+
+
+        //1.3.1.11 - mawq
+        //Console.Write("1-sonni kiriting : ");
+        //var firsValue = int.Parse(Console.ReadLine());
+        //Console.Write("2-sonni kiriting : ");
+        //var secondValue = int.Parse(Console.ReadLine());
+        //if (firsValue < secondValue)
+        //{
+        //    for (var i = firsValue; i <= secondValue; i++)
+        //    {
+        //        if (i % 4 == 0 && i % 7 == 0)
+        //        {
+        //            Console.Write($"{i} ");
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    Console.Write("1-son 2-sondan kichik bolsin !!!");
+        //}
+
+
+        //1.3.1.12 - mawq
+        //Console.Write("1-sonni kiriting : ");
+        //var firsValue = int.Parse(Console.ReadLine());
+        //Console.Write("2-sonni kiriting : ");
+        //var secondValue = int.Parse(Console.ReadLine());
+        //var total = 0;
+        //if (firsValue < secondValue)
+        //{
+        //    for (var i = firsValue; i <= secondValue; i++)
+        //    {
+        //        if (i % 8 == 0)
+        //        {
+        //            total++;
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    Console.Write("1-son 2-sondan kichik bolsin !!!");
+        //}
+        //Console.Write(total);
+
+
+        //1.3.1.13 - mawq
+        //Console.Write("1-sonni kiriting : ");
+        //var firsValue = int.Parse(Console.ReadLine());
+        //Console.Write("2-sonni kiriting : ");
+        //var secondValue = int.Parse(Console.ReadLine());
+        //if (firsValue < secondValue)
+        //{
+        //    for (var j = secondValue; j >= firsValue; j--)
+        //        if (j % 9 == 0 || j % 5 == 0)
+        //        {
+        //            Console.Write($"{j} ");
+        //        }
+
+        //}
+        //else
+        //{
+        //    Console.Write("1-son 2-sondan kichik bolsin !!!");
+        //}
+
+        //1.3.1.14 - mawq
+        //var total = 0;
+        //for (var i = 1; i <= 100; i++)
+        //{
+        //    if (i % 2 != 0)
+        //    {
+        //        total += i;
+        //    }
+        //}
+        //Console.Write($"Toq sonlarning jami yigindisi :{total} ga teng");
+
+
+        //1.3.1.15 - mawq
+        //for (var i = 100; i <= 999; i++)
+        //{
+        //    var r1 = i / 1 % 10;
+        //    var r2 = i / 10 % 10;
+        //    var r3 = i / 100 % 10;
+        //    if(r1+r2+r3 >= 20)
+        //    {
+        //        Console.Write($"{i} ");
+        //    }
+        //}
+
+
+        //1.3.1.16 - mawq
+        //for (var i = 100; i <= 999; i++)
+        //{
+        //    var r1 = i / 1 % 10;
+        //    var r2 = i / 10 % 10;
+        //    var r3 = i / 100 % 10;
+        //    if (r1 == r3 && r1 != r2)
+        //    {
+        //        Console.Write($"{i} ");
+        //    }
+        //}
+
+
+        //1.3.1.17 - mawq
+        //var total = 0;
+        //for (var i = 10; i <= 99; i++)
+        //{
+        //    var r1 = i / 1 % 10;
+        //    var r2 = i / 10 % 10;
+        //    if (r1 * r2 >= 12)
+        //    {
+        //        total += i;
+        //    }
+        //}
+        //Console.Write($"{total}");
+
+
+        //1.3.1.18 - mawq
+        //for (var i = 100; i <= 999; i++)
+        //{
+        //    var r2 = i / 10 % 10;
+        //    if ( r2 != 0 && r2 % 2 == 0 || r2 % 5 == 0)
+        //    {
+        //        Console.Write($"{i} ");
+        //    }
+        //}
+
+
+        //1.3.1.19 - mawq
+        //Console.Write("Son kiriting : ");
+        //var firstValue = int.Parse(Console.ReadLine());
+
+        //for(var i = 0; i <= firstValue; i++)
+        //{
+        //    Console.WriteLine($"{firstValue} ");
+        //}
+
+
+        //1.3.1.20 - mawq
+        //Console.Write("Son kiriting : ");
+        //var firstValue = int.Parse(Console.ReadLine());
+        //Console.Write("Son kiriting : ");
+        //var secondValue = int.Parse(Console.ReadLine());
+
+        //for (var i = 0; i < secondValue; i++)
+        //{
+        //    Console.WriteLine($"{firstValue} ");
+        //}
+
+
+        //1.3.1.21 - mawq
+        //Console.Write("Son kiriting : ");
+        //var firstValue = int.Parse(Console.ReadLine());
+        //Console.Write("Son kiriting : ");
+        //var secondValue = int.Parse(Console.ReadLine());
+
+        //for (var i = 0; i < firstValue; i++)
+        //{
+        //    Console.WriteLine($"{secondValue} ");
+        //}
+
+
+        //1.3.1.22 - mawq
+        //Console.Write("Son kiriting : ");
+        //var firstValue = int.Parse(Console.ReadLine());
+
+        //for (int i = 1; i <= firstValue; i++)
+        //{
+        //    if(firstValue % i == 0)
+        //    {
+        //        Console.Write($"{i} ");
+        //    }
+        //}
+
+
+        //1.3.1.23 - mawq
+        //Console.Write("Son kiriting : ");
+        //var firstValue = int.Parse(Console.ReadLine());
+
+        //var total = 0;
+        //for (int i = 1; i <= firstValue; i++)
+        //{
+        //    if (firstValue % i == 0)
+        //    {
+        //        total++;
+        //    }
+        //}
+        //Console.Write($"{total}");
+
+
+        //1.3.1.24 - mawq
+        //Console.Write("Son kiriting : ");
+        //var firstValue = int.Parse(Console.ReadLine());
+
+        //var total = 0;
+        //for (int i = 1; i <= firstValue; i++)
+        //{
+        //    if (firstValue % i == 0)
+        //    {
+        //        total += i;
+        //    }
+        //}
+        //Console.Write($"{total}");
+
+
+        //1.3.1.25 - mawq
+        //Console.Write("Sonni kiriting : ");
+        //var firstValue = int.Parse(Console.ReadLine());
+
+        //var total = 0;
+        //for (int i = 1; i <= firstValue; i++)
+        //{
+        //    if (firstValue % i == 0)
+        //    {
+        //        total++;
+        //    }
+        //}
+
+        //if (total == 2)
+        //{
+        //    Console.Write("True");
+        //}
+        //else
+        //{
+        //    Console.Write("False");
+        //}
+
+
+        //1.3.1.26 - mawq
+        //Console.Write("1 chis son : ");
+        //var num1 = int.Parse(Console.ReadLine());
+
+        //Console.Write("2 chis son : ");
+        //var num2 = int.Parse(Console.ReadLine());
+
+        //var num1Counter = 0;
+        //var num2Counter = 0;
+
+        //for (var i = 1; i <= num1; i++)
+        //{
+        //    if (num1 % i == 0)
+        //    {
+        //        num1Counter++;
+        //    }
+        //}
+
+        //for (var i = 1; i <= num2; i++)
+        //{
+        //    if (num2 % i == 0)
+        //    {
+        //        num2Counter++;
+        //    }
+        //}
+
+        //if (num1Counter == 2 && num2Counter == 2)
+        //{
+        //    Console.WriteLine(num1 + num2);
+        //}
+        //else
+        //{
+        //    Console.WriteLine(num1 * num2);
+        //}
+
+
+        //1.3.1.27 - mawq
+        //Console.Write("Sizda necha kg shakar bor : ");
+        //var num1 = int.Parse(Console.ReadLine());
+
+        //for (var i = 1; i <= num1; i++)
+        //{
+        //    Console.WriteLine($"{i} kg shakar {i*7000} som ");
+        //}
+
+
+
+        //1.3.1.28 - mawq
+        //Console.Write("Sonni kiriting : ");
+        //var num1 = int.Parse(Console.ReadLine());
+
+        //var total = 0;
+
+        //for (int i = 23; i < num1; i++) 
+        //{ 
+        //    if( i % 3 != 0) 
+        //    {
+        //        Console.WriteLine($"{i} ");
+        //        total++;
+        //    }        
+        //}
+        //Console.Write($"Ularning jami soni : {total}");
+
+
+        //1.3.1.28 - mawq
+        //Console.Write("1-Sonni kiriting : ");
+        //var num1 = int.Parse(Console.ReadLine());
+        //Console.Write("2-Sonni kiriting : ");
+        //var num2 = int.Parse(Console.ReadLine());
+
+        //var totalOne = 0;
+        //var totalTwo = 0;
+
+        //for (int i = 1; i <= num1; i++)
+        //{
+        //    if (num1 % i == 0) 
+        //    {
+        //      totalOne++;
+        //    }
+        //}
+
+        //for (int i = 1; i <= num2; i++)
+        //{
+        //    if (num2 % i == 0) 
+        //    {
+        //       totalTwo++;
+        //    }
+        //}
+
+        //if( totalOne > totalTwo)
+        //{
+        //    Console.Write($"Jami 1-sonda boluvchilar kop ular jami : {totalOne} ta");
+        //} else 
+        //{
+        //    Console.Write($"Jami 2-sonda boluvchilar kop ular jami : {totalTwo} ta");
+        //}
+
+
+
+        Console.WriteLine("1-sonni kiriting : ");
+        var firstValue = int.Parse(Console.ReadLine());
+        Console.WriteLine("2 sonni kiriting : ");
+        var secondValue = int.Parse(Console.ReadLine());
+
+
+        var total = 0;
+
+        for(var i = firstValue + 1; i < secondValue; i++)
+        {
+            total += i;
+        }
+
+        Console.Write($"{total}");
+    }
+}
